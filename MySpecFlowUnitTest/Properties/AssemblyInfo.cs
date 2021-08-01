22bf40ce6b1eb12274fcbe8514e99c9a6eeb89fd
yuhaoliao <h770320@gmail.com>
2021-08-01 09:22:01 +0800

    ++

---
+++ MySpecFlowUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MySpecFlowUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MySpecFlowUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf6c6934-d0f0-4cc7-bd8e-4ab9a5c876e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MySpecFlowUnitTest/Services/ParkingFeeService.cs
@@ -0,0 +1,64 @@
+﻿using MySpecFlowUnitTest.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySpecFlowUnitTest.Services
+{
+    public class ParkingFeeService : IParkingFeeService
+	{
+		int freeMinute = 10;
+		//計費單位(每60分鐘)
+		int unit = 60;
+		//每單位費用
+		int unitFee = 5;
+		//半個單位特殊處裡
+		int halfUnit = 30;
+		//半個單位費用
+		int halfUnitFee = 2;
+		//最大費用
+		int maxFee = 30;
+		public int Billing(int inputMinute)
+        {
+			int resultFee = 0;
+			int dayMinute = 60 * 24;
+			while (inputMinute > 0)
+			{
+				//每天拆開
+				int addDayMinute = inputMinute >= dayMinute ? dayMinute : inputMinute;
+				inputMinute -= addDayMinute;
+				 SingleDayBilling(addDayMinute);
+			}
+			return resultFee;
+		}
+		/// <summary>
+		/// 單日計算
+		/// </summary>
+		/// <param name="inputMinute"></param>
+		/// <returns></returns>
+		private int SingleDayBilling(int inputMinute)
+		{
+			//免費分鐘數內不用錢
+			if (freeMinute >= inputMinute)
+			{
+				return 0;
+			}
+			//不足一小時的時間
+			int remainderMinute = inputMinute % unit;
+			//幾個小時
+			int hour = inputMinute / unit;
+			//剩餘時間計費
+			int remainderMinuteCost = 0;
+			if (remainderMinute > 0)
+			{
+				remainderMinuteCost = remainderMinute > halfUnit ? unitFee : halfUnitFee;
+			}
+
+			int sumFee = hour * unitFee + remainderMinuteCost;
+
+			return sumFee > maxFee ? maxFee : sumFee;
+		}
+	}
+}
